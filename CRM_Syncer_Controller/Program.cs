90b550fc40a96917d50b9336fd8e8279a0161636
jposadafrz <james.posada@frezzor.com>
2015-07-03 10:27:28 -0700

    Added documention for each step of the controller]

--- CRM_Syncer_Controller/Program.cs
+++ CRM_Syncer_Controller/Program.cs
@@ -26,8 +26,13 @@ namespace CRM_Syncer_Controller
                 ShowWindow(hWnd, 0);
             }
 
+            // Step one we first find all new terminated accounts in Exigo with GUID's
             RemoveTerminatedAccounts RTA = new RemoveTerminatedAccounts();
+            // Step two we change the status and customer type of the account in CRM and remove the GUID from Exigo so changes to account
+            /// no longer affect crm.
             RTA.RemoveTermsNow();
+            /// Following 2 lines are step 3, here we are adding the guid's for all accounts in CRM that have exigo id numbers on them 
+            /// where CRM GUID (Other 8) is blank in Exigo this allows end users to add an ID number to a CRM account and the Syncer will populate/overwrite with Exigo Record.
             UpdateExigoGuids UEG = new UpdateExigoGuids();
             UEG.ExecuteUpdate();
             UpdateModifiedAccounts UMA = new UpdateModifiedAccounts();
